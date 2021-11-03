25805c2c20fce604ce61a4b215fb71ed9e617bb6
Nicholas Currault <nicholas.curr@gmail.com>
2021-11-02 22:08:40 -0700

    removed comments specific to python-telegram-bot

--- postgrespersistence.py
+++ postgrespersistence.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python
 #
-# A library that provides a Python interface to the Telegram Bot API
-# Copyright (C) 2015-2021
-# Leandro Toledo de Souza <devs@python-telegram-bot.org>
-#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
